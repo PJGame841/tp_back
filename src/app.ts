0368f3479be518e4957bb339c254f2aa789157fc
Pierre-Jean Lefort <pierrejean.lefort@itsap.com>
2024-04-09 16:47:07 +0200

    Listening

--- src/app.ts
+++ src/app.ts
@@ -27,6 +27,6 @@ swaggerLoder(app);
 
 const port = process.env.PORT ?? "3000";
 const host = process.env.HOST ?? "0.0.0.0";
-export const server = app.listen(parseInt(port), host, () => logger.log('info', `Listening on port ${port}`));
+export const server = app.listen(parseInt(port), host, () => logger.log('info', `Listening on ${host}:${port}`));
 
 export default app;
